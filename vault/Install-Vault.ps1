1913568cfcd95b1c0140a392a42eb85afc36dda9
James Anderton <janderton@hashicorp.com>
2020-06-09 23:01:34 -0500

    updated description and added path var and  config file section

--- vault/Install-Vault.ps1
+++ vault/Install-Vault.ps1
@@ -3,11 +3,18 @@
   Downloads and Installs Vault and Consul Binaries
   
   .DESCRIPTION
-   This script will download the requested Vault and Consul binaries, unzips and installs them to the chosen location
+   ###############################################################################################################################
+   #
+   #	 NAME: Install-Vault.ps1
+   #  Author: James Anderton <janderton@hashicorp.com>
+   #  Purpose: This script will download the requested Vault and Consul binaries, unzips and installs them to the chosen location
+   #
+   ###############################################################################################################################
   .Example: 
   .\Vault_Ent_Installer.ps1 -CONSUL_VERSION="1.7.3" -VAULT_VERSION="1.4.2"
 
 #>
+
 Param (
   
   [Parameter(
@@ -67,6 +74,10 @@ findstr (get-filehash -algorithm SHA256 ${CONSUL_DIR}\consul_${CONSUL_VERSION}_w
 # Expand out the zipfile to our directory
 Expand-Archive -Confirm -Force ${CONSUL_DIR}\consul_${CONSUL_VERSION}_windows_amd64.zip $CONSUL_DIR
 
+# Add Consul directory to the system path (both current and future)
+$env:path += ";${CONSUL_DIR}"
+[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";${CONSUL_DIR}", "Machine")
+
 # Set ProgressBar preference back to normal
 $ProgressPreference = $CurrentProgressPref;
 
@@ -101,3 +112,56 @@ Expand-Archive -Confirm -Force $ ${VAULT_DIR}\vault_${VAULT_VERSION}_windows_amd
 
 # Set ProgressBar preference back to normal
 $ProgressPreference = $CurrentProgressPref;
+
+######################################################################################################
+#
+# Create and configure Services    
+#
+######################################################################################################
+
+# Create consul-server.hcl
+New-Item -type file -Path $CONSUL_DIR -name consul-server.hcl
+
+$multiline_string = @'
+ This
+   is
+     my
+       System.String.
+'@
+
+echo $multiline_string > $CONSUL_DIR\consul-server.hcl
+
+# Create vault-server.hcl
+New-Item -type file -Path $VAULT_DIR -name vault-server.hcl
+
+$multiline_string = @'
+listener "tcp" {
+  address       = "0.0.0.0:8200"
+  tls_cert_file = "C:\\path\\to\\fullchain.pem"
+  tls_key_file  = "C:\\path\\to\\privkey.pem"
+}
+
+seal "pkcs11" {
+  lib            = "C:\\Vault\\cryptoki.dll"
+  slot           = "0"
+  pin            = "AAAA-BBBB-CCCC-DDDD"
+  key_label      = "vault-hsm-key"
+  hmac_key_label = "vault-hsm-hmac-key"
+}
+
+storage "raft" {
+  path = "$CONSUL_DIR\\data"
+  node_id = "raft_node_1"
+}
+
+#telemetry {
+#  statsite_address = "statsite.company.local:8125"
+#}
+
+#api_addr = "{{ full URL to Vault API endpoint }}"
+
+ui = true
+
+'@
+
+echo $multiline_string > $VAULT_DIR\vault-server.hcl
