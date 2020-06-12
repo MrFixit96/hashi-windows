ab739a9c98e2024e5f933fc0f95136416ae65f56
James Anderton <janderton@hashicorp.com>
2020-06-12 16:55:09 -0500

    Fixing Consul/Vault config files

--- vault/Install-Vault.ps1
+++ vault/Install-Vault.ps1
@@ -18,30 +18,28 @@
 Param (
   
   [Parameter(
-    Mandatory = $true,
     HelpMessage = 'CONSUL_VERSION is which version you want to download and install')]
   [string] $CONSUL_VERSION = '1.7.3',
  
   [Parameter(
     HelpMessage = 'CONSUL_URL is where to download Consul from. It defaults to https://releases.hashicorp.com/consul')]
-  [string] $CONSUL_URL = "https://releases.hashicorp.com/consul",
+  [string] $CONSUL_URL = 'https://releases.hashicorp.com/consul',
 
   [Parameter(
     HelpMessage = '# CONSUL_DIR is where to install Consul. Defaults to C:\Hashicorp\Consul')]
-  [string] $CONSUL_DIR = "C:\Program Files\Hashicorp\Consul",
+  [string] $CONSUL_DIR = "C:/Hashicorp/Consul",
 
   [Parameter(
-    Mandatory,
     HelpMessage = 'VAULT_VERSION is which version you want to download and install')]
   [string] $VAULT_VERSION = '1.4.2',
 
   [Parameter(
     HelpMessage = 'VAULT_URL is where to download Vault from. It defaults to https://releases.hashicorp.com/vault')]
-  [string] $VAULT_URL = "https://releases.hashicorp.com/Vault",
+  [string] $VAULT_URL = 'https://releases.hashicorp.com/vault',
 
   [Parameter(
     HelpMessage = 'VAULT_DIR is where to install Vault. Defaults to C:\\Program Files\\Hashicorp\\Vault')]
-  [string] $VAULT_DIR = "C:\\Program Files\\Hashicorp\\Vault"
+  [string] $VAULT_DIR = "C:/Hashicorp/Vault"
 )
 
 ##############################################################################################
@@ -51,11 +49,11 @@ Param (
 ##############################################################################################
 
 # Create Consul Directories to store binaries
-if (-not (Test-Path $CONSUL_DIR)){
-  mkdir $CONSUL_DIR
-  mkdir $CONSUL_DIR\data
+if (-not (Test-Path "$CONSUL_DIR")){
+  mkdir "$CONSUL_DIR"
+  mkdir "$CONSUL_DIR\data"
 }
-Set-Location $CONSUL_DIR
+Set-Location "$CONSUL_DIR"
 
 # Save Current ProgressBar Preference and set it to Silent because if we dont, 
 # Invoke-WebRequest blocks the stream to update the progress bar
@@ -69,10 +67,10 @@ Invoke-WebRequest "${CONSUL_URL}/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA2
 Invoke-WebRequest "${CONSUL_URL}/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS.sig" -Outfile consul_${CONSUL_VERSION}_SHA256SUMS.sig
 
 # Check the hashes to make sure we have valid files
-get-content ${CONSUL_DIR}\*SHA256SUMS | select-string  (get-filehash -algorithm SHA256 "${CONSUL_DIR}\consul_${CONSUL_VERSION}_windows_amd64.zip").hash.toLower()
+get-content "${CONSUL_DIR}\*SHA256SUMS"| select-string  (get-filehash -algorithm SHA256 "${CONSUL_DIR}\consul_${CONSUL_VERSION}_windows_amd64.zip").hash.toLower()
 
 # Expand out the zipfile to our directory
-Expand-Archive -Confirm -Force ${CONSUL_DIR}\consul_${CONSUL_VERSION}_windows_amd64.zip $CONSUL_DIR
+Expand-Archive -Confirm:$false -Force "${CONSUL_DIR}\consul_${CONSUL_VERSION}_windows_amd64.zip" "$CONSUL_DIR"
 
 # Add Consul directory to the system path (both current and future)
 $env:path += ";${CONSUL_DIR}"
@@ -88,11 +86,11 @@ $ProgressPreference = $CurrentProgressPref;
 ##############################################################################################
 
 # Create Consul Directories to store binaries
-if (-not (Test-Path $VAULT_DIR)){
-  mkdir $VAULT_DIR
-  mkdir $VAULT_DIR\data
+if (-not (Test-Path "$VAULT_DIR")){
+  mkdir "$VAULT_DIR"
+  mkdir "$VAULT_DIR\data"
 }
-Set-Location $VAULT_DIR
+Set-Location "$VAULT_DIR"
 
 # Save Current ProgressBar Preference and set it to Silent because if we dont, 
 # Invoke-WebRequest blocks the stream to update the progress bar
@@ -105,10 +103,10 @@ Invoke-WebRequest "${VAULT_URL}/${VAULT_VERSION}/vault_${VAULT_VERSION}_SHA256SU
 Invoke-WebRequest "${VAULT_URL}/${VAULT_VERSION}/vault_${VAULT_VERSION}_SHA256SUMS.sig" -Outfile vault_${VAULT_VERSION}_SHA256SUMS.sig
 
 # Check the hashes to make sure we have valid files
-get-content ${VAULT_DIR}\*SHA256SUMS | select-string  (get-filehash -algorithm SHA256 "${VAULT_DIR}\vault_${VAULT_VERSION}_windows_amd64.zip").hash.toLower()
+get-content "${VAULT_DIR}\*SHA256SUMS" | select-string  (get-filehash -algorithm SHA256 "${VAULT_DIR}\vault_${VAULT_VERSION}_windows_amd64.zip").hash.toLower()
 
 # Expand out the zipfile to our directory
-Expand-Archive -Confirm -Force $ ${VAULT_DIR}\vault_${VAULT_VERSION}_windows_amd64.zip $VAULT_DIR
+Expand-Archive -Confirm:$false -Force ${VAULT_DIR}\vault_${VAULT_VERSION}_windows_amd64.zip $VAULT_DIR
 
 # Add Vault directory to the system path (both current and future)
 $env:path += ";${VAULT_DIR}"
@@ -126,11 +124,11 @@ $ProgressPreference = $CurrentProgressPref;
 # Create consul-server.hcl
 New-Item -type file -Path $CONSUL_DIR -name consul-server.hcl
 
-$multiline_string = @'
-datacenter = "dc1"
+$multiline_string = @"
+datacenter = 'dc1'
 data_dir = "${CONSUL_DIR}\\data"
 
-#retry_join = ["xxx.xxx.xxx.xxx"]
+#retry_join = ['xxx.xxx.xxx.xxx']
 
 performance {
   raft_multiplier = 1
@@ -147,9 +145,9 @@ verify_outgoing = true
 verify_server_hostname = true
 
 ui = true
-client_addr = "0.0.0.0"
+client_addr = '0.0.0.0'
 
-'@
+"@
 
 echo $multiline_string > ${CONSUL_DIR}\consul-server.hcl
 
@@ -166,18 +164,20 @@ echo $multiline_string > ${CONSUL_DIR}\consul-acl.hcl
 
 # Create consul-telemetry.hcl
 $multiline_string = @'
-# UnComment These Lines to forward telemetry 
-#telemetry {
-#  metrics_path: '/v1/agent/metrics'
-#  params:
-#    format: ['prometheus']
-#
-#  statsite_address = "statsite.company.local:8125"
-#}
+// UnComment These Lines to forward telemetry 
+/*
+telemetry {
+  metrics_path: '/v1/agent/metrics'
+  params:
+    format: ['prometheus']
 
+  statsite_address = "statsite.company.local:8125"
+}
+*/
 '@
 
 echo $multiline_string > ${CONSUL_DIR}\consul-telemetry.hcl
+
 ############################
 # Create a unique, non-privileged system user to run consul.
 $Password = Read-Host -AsSecureString
@@ -191,23 +191,47 @@ New-Service -Name Consul -BinaryPathName "${CONSUL_DIR}\consul.exe -configdir=${
 # Create vault-server.hcl
 New-Item -type file -Path $VAULT_DIR -name vault-server.hcl
 
-$multiline_string = @'
+$multiline_string = @"
+
 listener "tcp" {
   address       = "0.0.0.0:8200"
-  tls_cert_file = "C:\\path\\to\\fullchain.pem"
-  tls_key_file  = "C:\\path\\to\\privkey.pem"
+  tls_cert_file = "${CONSUL_DIR}//fullchain.pem"
+  tls_key_file  = "${CONSUL_DIR}//privkey.pem"
 }
 
-#api_addr = "{{ full URL to Vault API endpoint }}"
+//api_addr = "{{ full URL to Vault API endpoint }}"
 
 ui = true
 
-'@
+# Disable Mlock because we're on Windows and it uses VirtualLock instead
+disable_mlock = true
 
-echo $multiline_string > ${VAULT_DIR}\vault-server.hcl
+"@
 
-# Create vault-seal.hcl
-$multiline_string = @'
+Add-Content -Path ${VAULT_DIR}\vault-server.hcl -Value $multiline_string
+
+# Add Cluster_Addr to vault-server.hcl
+$cluster_ip ="cluster_addr = `"https://" + (get-netipaddress -AddressFamily IPv4 -interfaceAlias Ethernet).ipaddress + "`""
+Add-Content -Path ${VAULT_DIR}\vault-server.hcl -Value $cluster_ip
+
+# Add api_addr to vault-server.hcl
+$api_ip ="api_addr = `"https://" + (get-netipaddress -AddressFamily IPv4 -interfaceAlias Ethernet).ipaddress + "`""
+Add-Content -Path ${VAULT_DIR}\vault-server.hcl -Value $cluster_ip
+
+# Create vault-storage.hcl
+$multiline_string = @"
+storage "raft" {
+  path = "${VAULT_DIR}/data"
+  node_id = "raft_node_1"
+}
+
+"@ 
+
+Add-Content -Path ${VAULT_DIR}\vault-storage.hcl -Value $multiline_string
+
+<# # Create vault-seal.hcl
+$multiline_string = @"
+/*
 seal "pkcs11" {
   lib            = "${VAULT_DIR}\\cryptoki.dll"
   slot           = "0"
@@ -215,32 +239,23 @@ seal "pkcs11" {
   key_label      = "vault-hsm-key"
   hmac_key_label = "vault-hsm-hmac-key"
 }
+*/
+    
+"@
 
-'@
-
-echo $multiline_string > ${VAULT_DIR}\vault-seal.hcl
-
-# Create vault-storage.hcl
-$multiline_string = @'
-storage "raft" {
-  path = "${VAULT_DIR}\\data"
-  node_id = "raft_node_1"
-}
-
-'@
-
-echo $multiline_string > ${VAULT_DIR}\vault-storage.hcl
+Add-Content -Path ${VAULT_DIR}\vault-seal.hcl -Value $multiline_string
 
 # Create vault-telemetry.hcl
 $multiline_string = @'
 # UnComment These Lines to forward telemetry 
-#telemetry {
-#  statsite_address = "statsite.company.local:8125"
-#}
+/*telemetry {
+  statsite_address = "statsite.company.local:8125"
+*/}
 
 '@
 
-echo $multiline_string > ${VAULT_DIR}\vault-telemetry.hcl
+Add-Content -Path ${VAULT_DIR}\vault-telemetry.hcl -Value $multiline_string
+#>
 
 ############################
 # Create a unique, non-privileged system user to run Vault.
@@ -259,8 +274,7 @@ $ProgressPreference = "SilentlyContinue";
 
 # Download NSSM
 Invoke-WebRequest http://nssm.cc/release/nssm-2.24.zip -Outfile "${home}\Downloads\nssm-2.24.zip"
-mkdir C:\Program Files\NSSM
-Expand-Archive -Force "${home}\Downloads\nssm-2.24.zip" "C:\Program Files\"
+Expand-Archive -Force "${home}\Downloads\nssm-2.24.zip" "C:\"
 
 $FileExe="${VAULT_DIR}\vault.exe"
 & "C:\Program Files\nssm-2.24\win64\nssm.exe" install Vault "$FileExe server -configdir=${VAULT_DIR}"
@@ -276,7 +290,7 @@ $FileExe="${VAULT_DIR}\vault.exe"
 
 # Execute Consul command to create a gossip key and store it in consul-server.hcl file
 $enc_key=(consul keygen)
-echo $enc_key >> ${CONSUL_DIR}\consul-server.hcl
+echo $enc_key >> "${CONSUL_DIR}\consul-server.hcl"
 
 # Execute Consul command to create the CA Cert we'll be building the rest of our certs from
 consul tls ca create
